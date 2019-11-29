5477653244bb8bb9f16d4550b68ebc662f6eb33b
ratov <ratov-kirill84@yandex.ru>
2019-11-29 18:19:07 +0300

    Отлавливаю ошибки

--- app.js
+++ app.js
@@ -85,10 +85,111 @@ btnAddPost.addEventListener('click', (e) => {
 
 
 
+function myHttpRequest({
+  method,
+  url
+} = {}, cb) {
+  try {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
 
+    xhr.addEventListener('load', () => {
+      if (Math.floor(xhr.status / 100) !== 2) {
+        cb(`Error. Status code: ${xhr.status}`, xhr);
+        return;
+      }
+      const response = JSON.parse(xhr.responseText);
+      cb(null, response);
+    });
 
+    xhr.addEventListener('error', () => {
+      cb(`Error. Status code: ${xhr.status}`, xhr);
+    });
 
+    xhr.send();
+  } catch (error) {
+    cb(error);
+  }
 
+}
+
+// myHttpRequest({
+//   method: 'GET',
+//   url: 'https://jsonplaceholder.typicode.com/posts'
+// }, (err, res) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(res);
+// });
+
+function http() {
+  return {
+    get(url, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        xhr.send();
+      } catch (error) {
+        cb(error);
+      }
+    },
+    post(url, body, headers, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        if (headers) {
+          Object.entries(headers).forEach(([key, value]) => {
+            xhr.setRequestHeader(key, value);
+          });
+        }
+
+        xhr.send(JSON.stringify(body));
+      } catch (error) {
+        cb(error);
+      }
+    },
+  };
+}
+const myHttp = http();
+myHttp.post('https://jsonplaceholder.typicode.com/posts', {
+  title: 'foo',
+  body: 'bar',
+  userId: 1,
+}, {
+  'Content-Type': 'application/json'
+}, (err, resp) => {
+  console.log(err, resp);
+});
 
 
 
